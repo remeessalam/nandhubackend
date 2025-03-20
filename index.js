fb2b76e2e16cdbe8305804749f79d3874aa05017
remeessalam <remeesreme4u@gmail.com>
2025-03-20 13:23:01 +0530

    added new server less http

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ app.use("/api/health", (req, res) => {
 
 app.use(errorHandler);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5050;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
-export const handler = serverless(app);
+module.exports.handler = serverless(app);
